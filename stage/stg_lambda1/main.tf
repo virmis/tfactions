faf3bbb78675024c6c3c21e64057160b607e2579
Virmantas Variakojis <virmantras.variakojis@gmail.com>
2020-08-02 14:52:39 +0300

    added one stg testint lambda

---
+++ stage/stg_lambda1/main.tf
@@ -0,0 +1,19 @@
+module "role" {
+  source = "../../modules/create_policy"
+
+  attach_policy_lambda_basic_execution_role = false
+}
+
+module "lambda" {
+  source  = "../../modules/create_lambda"
+
+  aws_region                  = "eu-central-1"
+  function_name               = "stg_lambda1"
+  description                 = "Test staging lambda 1"
+  function_source             = "src/lambda_function_payload.zip"
+  role_arn                    = module.role.lambda_role_arn
+  timeout                     = 3
+  add_cloudwatch_trigger      = false
+  parameter_store_path        = "/stage/lambda/ps-test"
+  env_vars                    = []
+}
